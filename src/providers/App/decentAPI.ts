6e8e069527445ee788e2e256d09c3753a689b021
Kellar <kellar.base09@gmail.com>
2025-06-26 14:35:48 +0100

    Rename useDecentApi to decentApi as its not a hook

--- src/providers/App/decentAPI.ts
+++ src/providers/App/decentAPI.ts
@@ -1,23 +1,49 @@
+// @todo this file should be replaced with decent-sdk package once it's ready
+
 import axios, { AxiosResponse } from 'axios';
 import { Address } from 'viem';
-import { StakingTokenData } from '../../../types/revenueSharing';
+import { StakingTokenData } from '../../types/revenueSharing';
 
 const DECENT_API_BASE_URL = 'https://api.decent.build';
 
 const axiosClient = axios.create({ baseURL: DECENT_API_BASE_URL });
 
-interface DAO {
+interface DAOBasic {
   name: string;
   address: Address;
   chainId: number;
 }
 
-interface DAOQueryResponse {
-  success: boolean;
-  data: DAO[];
+interface Token {
+  address: Address;
+  symbol: string;
+  name: string;
+  balance: string;
+  decimals: number;
 }
 
-// @todo this file should be replaced with decent-sdk package once it's ready
+interface DAOWithTokens extends DAOBasic {
+  token: Token;
+  stakedToken?: Token;
+}
+
+interface DAOQueryResponse {
+  success: boolean;
+  data: DAOBasic[];
+}
+
+export async function getDaoData(chainId: number, daoAddress: Address) {
+  const response: AxiosResponse<{ success: boolean; data: DAOWithTokens }> = await axiosClient.get(
+    `/d/${chainId}/${daoAddress}`,
+  );
+
+  if (!response.data.success) {
+    return null;
+  }
+
+  return response.data.data;
+}
+
 export async function queryDaosByName(name: string) {
   const response: AxiosResponse<DAOQueryResponse> = await axiosClient.get('/d', {
     params: { name },
