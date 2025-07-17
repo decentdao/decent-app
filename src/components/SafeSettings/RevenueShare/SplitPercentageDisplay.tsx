8038264eb37ab6ac5708af17ebd7071d05e80680
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-07-17 10:07:58 -0400

    add new SplitPercentageDisplayComponent

---
+++ src/components/SafeSettings/RevenueShare/SplitPercentageDisplay.tsx
@@ -0,0 +1,31 @@
+import { Flex, Icon, Text } from '@chakra-ui/react';
+import { CheckCircle, WarningCircle } from '@phosphor-icons/react';
+
+export function SplitPercentageDisplay({ percentage }: { percentage: number }) {
+  const isBelowZero = percentage < 0;
+  const isAboveOneHundred = percentage > 100;
+  const isPercentageValid = !isBelowZero && !isAboveOneHundred;
+
+  const iconColor = isPercentageValid ? 'color-success-400' : 'color-error-400';
+  const icon = isPercentageValid ? CheckCircle : WarningCircle;
+
+  const text = isPercentageValid ? `${percentage}%` : `${percentage}% Total must equal 100%`;
+
+  return (
+    <Flex
+      alignItems="center"
+      gap="0.5rem"
+    >
+      <Icon
+        as={icon}
+        color={iconColor}
+      />
+      <Text
+        textStyle="text-sm-leading-none-medium"
+        color="color-content-popover-foreground"
+      >
+        {text}
+      </Text>
+    </Flex>
+  );
+}
