e5473cfbdf44a420a78091fabc684e30916e4c7d
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-08-18 13:59:53 -0400

    refactor: remove unused ROLES import from SafeProposalTemplatesPage

--- src/pages/dao/proposal-templates/SafeProposalTemplatesPage.tsx
+++ src/pages/dao/proposal-templates/SafeProposalTemplatesPage.tsx
@@ -14,7 +14,6 @@ import { ModalType } from '../../../components/ui/modals/ModalProvider';
 import { useDecentModal } from '../../../components/ui/modals/useDecentModal';
 import PageHeader from '../../../components/ui/page/Header/PageHeader';
 import Divider from '../../../components/ui/utils/Divider';
-import { ROLES } from '../../../constants/accessControlRoles';
 import { DAO_ROUTES } from '../../../constants/routes';
 import { useCurrentDAOKey } from '../../../hooks/DAO/useCurrentDAOKey';
 import useSendAssetsActionModal from '../../../hooks/DAO/useSendAssetsActionModal';
