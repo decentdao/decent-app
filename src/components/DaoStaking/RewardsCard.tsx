c0d9e6bfac4a851649ae043b571a0d78c6022684
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-08-25 10:52:27 -0400

    Update rewards token card with live data

--- src/components/DaoStaking/RewardsCard.tsx
+++ src/components/DaoStaking/RewardsCard.tsx
@@ -1,7 +1,123 @@
-import { Flex, Text } from '@chakra-ui/react';
-import { CaretDown } from '@phosphor-icons/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import { CaretDown, CaretUp } from '@phosphor-icons/react';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import {  useDurationDisplay } from '../../helpers/dateTime';
+import { useCurrentDAOKey } from '../../hooks/DAO/useCurrentDAOKey';
+import { useDAOStore } from '../../providers/App/AppProvider';
+import { formatUSD } from '../../utils';
+import Divider from '../ui/utils/Divider';
+
+function ViewTokens({ numOfTokens }: { numOfTokens: number }) {
+  const { t } = useTranslation('staking');
+  
+  return (
+    <Flex
+      direction="column"
+      justifyContent="center"
+      alignItems="flex-start"
+      flex="1 0 0"
+    >
+      <Text
+        alignSelf="stretch"
+        color="color-content-content1-foreground"
+        textStyle="text-sm-regular"
+      >
+        {numOfTokens === 1
+          ? t('viewToken')
+          : numOfTokens > 1
+            ? t('viewTokens', { count: numOfTokens })
+            : t('noRewardTokens')}
+      </Text>
+    </Flex>
+  );
+}
+
+function RewardsTokens() {
+  const { daoKey } = useCurrentDAOKey();
+  const {
+    governance: { stakedToken },
+  } = useDAOStore({ daoKey });
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <Flex
+      minHeight="40px"
+      padding="0px 16px"
+      direction="column"
+      justifyContent="center"
+      alignItems="flex-start"
+      alignSelf="stretch"
+      borderRadius="12px"
+      border="1px solid var(--colors-color-layout-border)"
+      background="color-alpha-black-950"
+      onClick={() => setExpanded(!expanded)}
+      aria-label="View 3 Tokens"
+    >
+      {!expanded ? (
+        <Flex
+          padding="16px 0px"
+          justifyContent="space-between"
+          alignItems="center"
+          alignSelf="stretch"
+        >
+          <ViewTokens numOfTokens={stakedToken?.rewardsTokens.length ?? 0} />
+          <CaretDown />
+        </Flex>
+      ) : (
+        <>
+          <Flex
+            padding="16px 0px"
+            justifyContent="space-between"
+            alignItems="center"
+            alignSelf="stretch"
+          >
+            <ViewTokens numOfTokens={stakedToken?.rewardsTokens.length ?? 0} />
+            <CaretUp />
+          </Flex>
+          {stakedToken?.rewardsTokens.map((token, index, arr) => (
+            <Flex
+              key={index}
+              direction="column"
+              alignSelf="stretch"
+              >
+              <Flex
+                gap="4px"
+                justifyContent="space-between"
+                alignSelf="stretch"
+                mb="8px"
+              >
+                <Text
+                  textStyle="text-sm-regular"
+                  color="color-layout-foreground"
+                >
+                  {token.symbol}
+                </Text>
+                <Text
+                  textStyle="text-xs-regular"
+                  color="color-content-muted"
+                >
+                  {token.formattedBalance}
+                </Text>
+              </Flex>
+              {index !== arr.length - 1 && index !== 1 && <Divider variant="darker" mb="8px"/>}
+            </Flex>
+          ))}
+        </>
+      )}
+    </Flex>
+  );
+}
 
 export default function RewardsCard() {
+  const { t } = useTranslation('staking');
+  const { daoKey } = useCurrentDAOKey();
+  const {
+    governance: { stakedToken },
+  } = useDAOStore({ daoKey });
+
+  const totalRewards = stakedToken?.rewardsTokens.reduce((acc, token) => acc + token.usdValue, 0) ?? 0;
+  const lockPeriod = useDurationDisplay(stakedToken?.minimumStakingPeriod)
   return (
     <Flex
       padding="14px 12px"
@@ -29,93 +145,25 @@ export default function RewardsCard() {
             color="color-charcoal-400"
             textStyle="labels-large"
           >
-            Rewards Overview
+            {t('rewardsOverview')}
           </Text>
         </Flex>
 
         <Flex
           justifyContent="space-between"
-          alignItems="flex-start"
+          alignItems="center"
           alignSelf="stretch"
         >
           <Text
             color="color-layout-foreground"
             textStyle="text-2xl-regular"
           >
-            $2.46K
+            {formatUSD(totalRewards)}
           </Text>
-          <Flex
-            height="36px"
-            padding="0px 16px"
-            justifyContent="center"
-            alignItems="center"
-            gap="8px"
-            borderRadius="8px"
-            borderTop="1px solid var(--colors-color-layout-border-primary)"
-            background="color-base-primary"
-          >
-            <Flex
-              justifyContent="center"
-              alignItems="center"
-              gap="8px"
-            >
-              <Text
-                color="color-base-primary-foreground"
-                textStyle="text-sm-regular"
-              >
-                Claim All
-              </Text>
-            </Flex>
-          </Flex>
+          <Button>{t('claimAll')}</Button>
         </Flex>
 
-        <Flex
-          direction="column"
-          alignItems="flex-start"
-          gap="8px"
-          alignSelf="stretch"
-        >
-          <Text
-            color="color-content-content4-foreground"
-            textStyle="text-sm-regular"
-          >
-            Pending Rewards
-          </Text>
-          <Flex
-            height="40px"
-            padding="0px 16px"
-            direction="column"
-            justifyContent="center"
-            alignItems="flex-start"
-            alignSelf="stretch"
-            borderRadius="12px"
-            border="1px solid var(--colors-color-layout-border)"
-            background="color-alpha-black-950"
-          >
-            <Flex
-              padding="16px 0px"
-              justifyContent="space-between"
-              alignItems="center"
-              alignSelf="stretch"
-            >
-              <Flex
-                direction="column"
-                justifyContent="center"
-                alignItems="flex-start"
-                flex="1 0 0"
-              >
-                <Text
-                  alignSelf="stretch"
-                  color="color-content-content1-foreground"
-                  textStyle="text-sm-regular"
-                >
-                  View 3 Tokens
-                </Text>
-              </Flex>
-              <CaretDown />
-            </Flex>
-          </Flex>
-        </Flex>
+        <RewardsTokens />
       </Flex>
 
       <Flex
@@ -133,13 +181,13 @@ export default function RewardsCard() {
             color="color-content-content1-foreground"
             textStyle="text-sm-regular"
           >
-            Lock Period
+            {t('lockPeriod')}
           </Text>
           <Text
             color="color-content-content4-foreground"
             textStyle="text-sm-regular"
           >
-            30 Days
+            {lockPeriod}
           </Text>
         </Flex>
       </Flex>
