bd92293a1af62d21efffe55ea1ce0379e51f7bd7
twodam <twodam@protonmail.com>
2025-09-01 16:00:58 +0800

    refactor(TransactionBuilderModal): remove unused form; rename and move as modal

--- src/components/ui/modals/TransactionBuilderModal.tsx
+++ src/components/ui/modals/TransactionBuilderModal.tsx
@@ -1,66 +1,25 @@
 import { Box, Button } from '@chakra-ui/react';
 import { Plus } from '@phosphor-icons/react';
-import { Formik, FormikProps } from 'formik';
+import { Formik } from 'formik';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import useCreateProposalSchema from '../../hooks/schemas/proposalBuilder/useCreateProposalSchema';
-import { CreateProposalTransaction } from '../../types/proposalBuilder';
-import { scrollToBottom } from '../../utils/ui';
-import CeleryButtonWithIcon from '../ui/utils/CeleryButtonWithIcon';
-import Divider from '../ui/utils/Divider';
-import ProposalTransactions from './ProposalTransactions';
-import { DEFAULT_PROPOSAL_TRANSACTION } from './constants';
+import useCreateProposalSchema from '../../../hooks/schemas/proposalBuilder/useCreateProposalSchema';
+import { CreateProposalTransaction } from '../../../types/proposalBuilder';
+import { scrollToBottom } from '../../../utils/ui';
+import ProposalTransactions from '../../ProposalBuilder/ProposalTransactions';
+import { DEFAULT_PROPOSAL_TRANSACTION } from '../../ProposalBuilder/constants';
+import CeleryButtonWithIcon from '../utils/CeleryButtonWithIcon';
+import Divider from '../utils/Divider';
 
 export interface ProposalTransactionsFormProps {
   pendingTransaction: boolean;
   isProposalMode: boolean;
-  setFieldValue: FormikProps<CreateProposalTransaction[]>['setFieldValue'];
-  values: FormikProps<CreateProposalTransaction[]>['values'];
-  errors?: FormikProps<CreateProposalTransaction[]>['errors'];
   onSubmit?: (txs: CreateProposalTransaction[]) => void;
   onClose?: () => void;
   submitButtonText?: string;
 }
 
-export default function ProposalTransactionsForm(props: ProposalTransactionsFormProps) {
-  const { pendingTransaction, setFieldValue, values } = props;
-  const { t } = useTranslation(['proposal']);
-  const [expandedIndecies, setExpandedIndecies] = useState<number[]>([0]);
-
-  const removeTransaction = (index: number) => {
-    const allTxs = [...values];
-    allTxs.splice(index, 1);
-    setFieldValue('transactions', allTxs);
-    setExpandedIndecies(prev => prev.filter(i => i !== index));
-  };
-
-  return (
-    <Box py="1.5rem">
-      <ProposalTransactions
-        {...props}
-        expandedIndecies={expandedIndecies}
-        setExpandedIndecies={setExpandedIndecies}
-        removeTransaction={removeTransaction}
-        setFieldValue={(field: string, value: unknown) => {
-          setFieldValue(`transactions.${field}`, value);
-        }}
-      />
-      <Divider my="1.5rem" />
-      <CeleryButtonWithIcon
-        onClick={() => {
-          setFieldValue('transactions', [...values, DEFAULT_PROPOSAL_TRANSACTION]);
-          setExpandedIndecies([values.length]);
-          scrollToBottom(100, 'smooth');
-        }}
-        isDisabled={pendingTransaction}
-        icon={Plus}
-        text={t('labelAddTransaction')}
-      />
-    </Box>
-  );
-}
-
-export function ProposalTransactionsFormModal({
+export function TransactionBuilderModal({
   pendingTransaction,
   isProposalMode,
   onSubmit,
