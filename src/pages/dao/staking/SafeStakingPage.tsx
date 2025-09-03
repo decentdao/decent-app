972969426ce36cfaf8535b5b8084d5b9d03a06ff
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-09-03 15:18:28 -0400

    chore: remove unused Divider import from SafeStakingPage

--- src/pages/dao/staking/SafeStakingPage.tsx
+++ src/pages/dao/staking/SafeStakingPage.tsx
@@ -1,5 +1,5 @@
 import * as amplitude from '@amplitude/analytics-browser';
-import { Box, Divider, Flex, Grid } from '@chakra-ui/react';
+import { Box, Flex, Grid } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import BalanceCard from '../../../components/DaoStaking/BalanceCard';
